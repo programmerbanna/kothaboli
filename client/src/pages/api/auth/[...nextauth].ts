f56c03a0e55e5fc64596313ce2cbe846deeb5cd4
programmerbanna <programmerbanna@gmail.com>
2022-10-14 19:58:27 +0600

    prisma schema reconfigured for mongodb

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../libs/prismadb";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
